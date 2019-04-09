25e660c7918d83bb9400c14258f2eb53a780cbe4
lauragreen <lollymay@gmail.com>
2019-04-09 19:57:38 +0100

    Add Query & PlantListItem component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./App.css";
 
+const PLANT = gql`
+  {
+    Plants {
+      link
+      scientific_name
+      slug
+    }
+  }
+`;
+
+function PlantListItem({ link, scientific_name }) {
+  return (
+    <li>
+      <a href={link}>{scientific_name}</a>
+    </li>
+  );
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Query query={PLANT}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            console.log(data);
+            return data.Plants.map(plant => (
+              <PlantListItem
+                link={plant.link}
+                scientific_name={plant.scientific_name}
+                key={plant.slug}
+              />
+            ));
+          }}
+        </Query>
       </div>
     );
   }
